b828b3f39f5c7c2e9c4f94510e65a0d0f971ecf8
afroprogrammer <forsonfranklin@gmail.com>
2021-05-26 23:13:30 +0000

    fixed bugs

--- src/main/java/com/example/springbootcrudapplication/controller/ProductController.java
+++ src/main/java/com/example/springbootcrudapplication/controller/ProductController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 @Controller
@@ -34,5 +36,16 @@ public class ProductController {
         return "redirect:/";
 
   }
-
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductForm(@PathVariable(name = "id") Long id) {
+   ModelAndView mav = new ModelAndView("edit_product");
+   Product product = productService.get(id);
+   mav.addObject("product", product);
+        return mav;
+    }
+    @RequestMapping("/delete/{id}")
+    public  String deleteProduct(@PathVariable(name = "id") Long id){
+        productService.delete(id);
+        return "redirect:/";
+    }
 }
