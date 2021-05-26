7bf83f58e2e99e369bc7360edb2788a53bff292d
afroprogrammer <forsonfranklin@gmail.com>
2021-05-26 16:27:29 +0000

    created a rest controller to get the index page

---
+++ src/main/java/com/example/springbootcrudapplication/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.example.springbootcrudapplication.controller;
+
+import com.example.springbootcrudapplication.model.Product;
+import com.example.springbootcrudapplication.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping("/")
+    public String viewHomepage(Model model){
+        List<Product> listProducts = productService.listAll();
+        model.addAttribute("listProducts", listProducts);
+        return "index";
+    }
+
+
+
+}
