fac46047677dd8c9550b494bc84194fd97f94a30
afroprogrammer <forsonfranklin@gmail.com>
2021-05-26 19:35:54 +0000

    fixed bugs

--- src/main/java/com/example/springbootcrudapplication/controller/ProductController.java
+++ src/main/java/com/example/springbootcrudapplication/controller/ProductController.java
@@ -3,11 +3,12 @@ package com.example.springbootcrudapplication.controller;
 import com.example.springbootcrudapplication.model.Product;
 import com.example.springbootcrudapplication.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+@Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
